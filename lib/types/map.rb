fefdbe1fc575970a7aed438756eb679484199978
Martin Becker <mbeckerwork@gmail.com>
2017-08-14 09:25:33 +0100

    removes initializer and now uses field method to set field defaults

--- lib/types/map.rb
+++ lib/types/map.rb
@@ -5,15 +5,15 @@ module Map_Generator
       fields size_x: Integer,size_y: Integer, max_height: Integer
       fields tile_map: Tiles
 
-      def initialize(from_file=false)
-        unless from_file
-          @size_x = 100
-          @size_y = 100
-          @max_height = 256
-          @tile_map =  Array.new(@size_x,Array.new(@size_y,[]))
-        end
-      end
+      field :size_x, type: Integer, default: 100
+      field :size_y, type: Integer, default: 100
+      field :max_height, type: Integer, default: 256
+      field :Height_Map, Type: Integer, container: Array, default: -> {
+        Array.new(@size_x,Array.new(@size_y,[]))
+      }
 
+
+     
     end
   end
 end
