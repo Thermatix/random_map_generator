1f2434b4590d716062b47a99951a5845bf68a8a4
Martin Becker <mbeckerwork@gmail.com>
2017-08-14 15:55:50 +0100

    git seems to think it's differant, it's not

--- lib/auto_loader.rb
+++ lib/auto_loader.rb
@@ -4,15 +4,14 @@
 end
 class Object
   def self.inherited(base)
-    puts base
     base.extend(Class_Methods)
     super
   end
 
   module Class_Methods
   
-    def const_missing(name)
-      puts "loading %s" % name
+    def const_missing(const)
+      puts "loading %s" % name.to_s
       path = name.to_s.split('::').compact.map(&:downcase).join('/') 
       require path
       self.const_get(name)
