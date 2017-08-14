a5ea2901578585879e5dd43b4001f0323d3281a9
Martin Becker <mbeckerwork@gmail.com>
2017-08-14 09:07:45 +0100

    adds constructor

--- lib/types/map.rb
+++ lib/types/map.rb
@@ -3,7 +3,17 @@ module Map_Generator
     class Map
       include Format    
       fields size_x: Integer,size_y: Integer, max_height: Integer
-      fields tile_map: [Tiles]
+      fields tile_map: Tiles
+
+      def initialize(from_file=false)
+        unless from_file
+          @size_x = 100
+          @size_y = 100
+          @max_height = 256
+          @tile_map =  Array.new(@size_x,Array.new(@size_y,[]))
+        end
+      end
+
     end
   end
 end
