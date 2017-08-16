486c5b51904faffa6a09feaf484d3da6af5f9685
Martin Becker <mbeckerwork@gmail.com>
2017-08-16 10:10:36 +0100

    changes how default value for map is set

--- lib/types/map.rb
+++ lib/types/map.rb
@@ -9,28 +9,37 @@ module Map_Generator
       field :size_x, type: Integer, default: 15
       field :size_y, type: Integer, default: 30
       field :max_height, type: Integer, default: 256
-      field :height_Map, type: Integer, container: Array, accessors: false, default: -> {
-        Array.new(@size_x,Array.new(@size_y,0))
+      field :height_map, type: Integer, container: Array, accessors: false, default: -> {
+        Array.new(@size_x) {Array.new(@size_y,0)}
       }
 
     def [](index)
-      @height_Map[index]
+      @height_map[index]
     end
 
     def []=(index,val)
-      @height_Map[index] = val
+      @height_map[index] = val
     end
 
     def display
-      colorise.each do |row|
+      colorise_map.each do |row|
         puts row.join('')
       end
     end
 
-    def colorise
-      @height_Map.map do |row|
-        row.map do |col|
-          case true
+    def display_row(x)
+      puts colorise_row(@height_map[x]).join('')
+    end
+  
+    def colorise_map
+      @height_map.map do |row|
+        colorise_row(row)
+      end
+    end
+
+    def colorise_row(row)
+      row.map do |col|
+        case true
           when col > 64
             ' '.colorize(:white).on_light_green
           when col > 32
@@ -39,7 +48,6 @@ module Map_Generator
             '\\'.colorize(:light_yellow).on_yellow
           else
             '~'.colorize(:white).on_light_blue
-          end
         end
       end
     end
