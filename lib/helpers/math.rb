5330183c96df6b64ba4e0d6a181471bec850cae4
Martin Becker <mbeckerwork@gmail.com>
2017-08-17 10:14:26 +0100

    adds Max,Min function to math module

--- lib/helpers/math.rb
+++ lib/helpers/math.rb
@@ -1,6 +1,11 @@
 module Map_Generator
   module Helpers
     module Math
+
+      def self.included(base)
+        base.extend(self)
+      end
+
       def Sum(values)
         values.inject(0.0) { |result, el| result + el }
       end
@@ -9,6 +14,17 @@ module Map_Generator
         v = values.flatten
         res = (Sum(v) / v.size).floor
         (res == [] ? 0 : res).abs
+      end
+
+
+      def Max(max)
+        val = yield
+        val > max ? max : val
+      end
+
+      def Min(min=0)
+        val = yield
+        val < min ? min : val
       end
 
     end
