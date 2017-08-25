8f7679d5cdd2d2fd27486ffc1ab6508ff3248772
Martin Becker <mbeckerwork@gmail.com>
2017-08-25 11:06:14 +0100

    adds MaxMin and Median math functions

--- lib/helpers/math.rb
+++ lib/helpers/math.rb
@@ -6,8 +6,8 @@ module Map_Generator
         base.extend(self)
       end
 
-      def Sum(values)
-        values.inject(0.0) { |result, el| (result||0) + (el||0) }
+      def Sum(*values)
+        values.flatten.inject(0.0) { |result, el| (result||0) + (el||0) }
       end
 
       def Mean(*values)
@@ -16,17 +16,28 @@ module Map_Generator
         (res == [] ? 0 : res).abs
       end
 
+      def Median(*values)
+        sorted = values.flatten.sort
+        len = sorted.length
+        (sorted[(len - 1) / 2] + sorted[len / 2]) / 2.0
+      end
 
-      def Max(max)
-        val = yield
+      def Max(max,val=nil,&block)
+        val = val || block.call
         val > max ? max : val
       end
 
-      def Min(min=0)
-        val = yield
-        puts "%s/%s" % [val,min]
+      def Min(min=0,val=nil,&block)
+        val = val || block.call
         val < min ? min : val
       end
+
+      def MaxMin(max,min=0)
+        val = yield
+        return val if val == Max(max,val) && val == Min(min,val)
+        val < Median(((min)..(max)).to_a) ? min : max
+      end
+
 
     end
   end
