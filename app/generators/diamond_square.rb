98a50113accb65766ba2edb5fd6de36b7c80200a
Martin Becker <mbeckerwork@gmail.com>
2017-08-25 11:06:30 +0100

    changes the algorithm a 4th time

--- app/generators/diamond_square.rb
+++ app/generators/diamond_square.rb
@@ -10,7 +10,7 @@ module Map_Generator
             map_array[Max(map_array.mx){x}][Min(){y - level.y}]
 					},
 					left:		->	(map_array,x,y,level) {
-            map_array[Min(){x - level.x}][Max(map_array.my){y}]
+            map_array[MaxMin(map_array.mx){x - level.x}][Max(map_array.my){y}]
 					},
 					bottom: ->	(map_array,x,y,level) {
             map_array[Max(map_array.mx){x}][Max(map_array.my ){y + level.y}]
@@ -21,34 +21,49 @@ module Map_Generator
 				},
 				square: {
 					tp_left:		-> (map_array,x,y,level) {
-            map_array[Min(){x - level.x}][Min(){y - level.y}]
+            map_array[MaxMin(map_array.mx){x - level.x}][MaxMin(map_array.my){y - level.y}]
 					},
 					tp_right:		-> (map_array,x,y,level) {
-            map_array[Max(map_array.mx){x + level.x}][Min(){y - level.y}]
+            map_array[MaxMin(map_array.mx){x + level.x}][MaxMin(map_array.my){y - level.y}]
 					},
 					btm_left:		-> (map_array,x,y,level) {
-            map_array[Min(){x - level.x}][Max(map_array.my){y + level.y}]
+            map_array[MaxMin(map_array.mx){x - level.x}][MaxMin(map_array.my){y + level.y}]
 					},
 					btm_right:	-> (map_array,x,y,level) {
-            map_array[Max(map_array.mx){x + level.x}][Max(map_array.my){y + level.y}]
+            map_array[MaxMin(map_array.mx){x + level.x}][MaxMin(map_array.my){y + level.y}]
 					}
 				}
 			}
+
+      Offset = {
+        # top: -> (x,y,level) {
+        #   [x,y - level.y]
+        # }#,
+        # left: -> (x,y,level) {
+        #   [x - level.x,y]
+        # }#,
+        # right: -> (x,y,level) {
+        #   [x + level.x,y]
+        # },
+        # bottom: -> (x,y,level) {
+        #   [x,y + level.y ]
+        # }#,
+      }
 
       def main(map_array)
         ground_level = map_array.max_height / 8
         fuzzy = map_array.max_height / 16 
         level = Dot({
-          x: (map_array.size_x ) / 2,
-          y: (map_array.size_y ) / 2
+          x: (map_array.mx) / 2,
+          y: (map_array.my) / 2
         })
         fzy = rand_value(ground_level,fuzzy)
         seed_corner(map_array,ground_level,fuzzy)
         cords = Dot({
-          x1:0,
-          y1:0,
-          x2:map_array.size_x,
-          y2:map_array.size_y
+          x1: 0,
+          y1: 0,
+          x2: map_array.mx,
+          y2: map_array.my
         })
         @step = 1
         step(map_array,level,fzy,cords)
@@ -56,6 +71,7 @@ module Map_Generator
       end
 
       def step(map_array,level,fzy,cords)
+        map_array.display
         return map_array unless level.x > 0 && level.y > 0
         diamond_step(map_array,level,fzy,cords)
         square_step(map_array,level,fzy,cords)
@@ -73,23 +89,37 @@ module Map_Generator
 
 
       def diamond_step(map_array,level,fzy,cords)
-        iteration(cords,level) do |x,y|
-          avg = Mean(get_shape_values(:square,map_array,x,y,level))
-          map_array[Max(map_array.mx){x}][Max(map_array.my){y}] = avg + rand_value(avg)
+        # iteration(cords,level) do |x,y|
+        (1..(cords.x2 / level.x)).each do |xs,x=(xs * level.x)|
+          (1..(cords.y2 / level.y)).each do |ys,y=(xs * level.x)|
+            avg = Mean(get_shape_values(:square,map_array,x,y,level))
+            map_array[Max(map_array.mx){x}][Max(map_array.my){y}] = avg + rand_value(avg)
+          end
         end
       end
 
       def square_step(map_array,level,fzy,cords)
-        iteration(cords,level,0) do |x,y,x_off=(x + (level[:x] / 2)),y_off=(y + (level[:y] / 2))|
-          avg = Mean(get_shape_values(:diamond,map_array,x_off,y_off,level))
-          map_array[Max(map_array.mx){x_off}][Max(map_array.my){y_off}] = avg + rand_value(avg)
+        (0..(cords.x2 / level.x)).each do |xs,x=(xs * level.x)|
+          y = (x + (level.y / 2)) % level.x
+          while y <= map_array.my do
+            avg = Mean(get_shape_values(:diamond,map_array,x,y,level))
+            map_array[Max(map_array.mx){x}][Max(map_array.my){y}] = avg + rand_value(avg)
+            y += level.y
+          end
         end
+        #   Offset.each do |_,off_set_calc|
+        #     x_off,y_off = off_set_calc.call(x,y,level)
+        #     puts "%s,%s|%s,%s" % [x,y,x_off,y_off]
+        #     avg = Mean(get_shape_values(:diamond,map_array,x_off,y_off,level))
+        #     map_array[Max(map_array.mx){x_off}][Max(map_array.my){y_off}] = avg + rand_value(avg)
+        #   end
+        # end
       end
 
       def iteration(cords,level,start=1)
         unless level.x < 1 || level.y < 1
-          (start..(cords.x2 / level.x)).each do |xs,x=(xs * level[:x])|
-            (start..(cords.y2 / level.y)).each do |ys,y=(ys * level[:y])|
+          (start..(cords.x2 / level.x)).each do |xs,x=(xs * level.x)|
+            (start..(cords.y2 / level.y)).each do |ys,y=(ys * level.y)|
               yield(x,y)
             end
           end
